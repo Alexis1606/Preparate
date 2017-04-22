18ea54a494e1be1d49ae1d57de0388b1ceb951f9
Juan Luis Aguilar Badillo <jhony_primitivo@hotmail.com>
2017-04-22 08:35:34 -0500

    Acceder

--- preparate/Acceder.cs
+++ preparate/Acceder.cs
@@ -15,16 +15,67 @@ namespace preparate
     [Activity(Label = "Acceder")]
     public class Acceder : Activity
     {
+        EditText Correo;
+        EditText Contrasena;
+        Button bAcceder;
+
+        string correo2;
+        string contrasena2;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            EditText Correo;
-            EditText Contrasena;
             
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Acceder);
+            Correo = FindViewById<EditText>(Resource.Id.txtEmail);
             Contrasena = FindViewById<EditText>(Resource.Id.txtPassword);
+            bAcceder = FindViewById<Button>(Resource.Id.txtAcceder);
+            bAcceder.Click += bAcceder_Click;
 
             // Create your application here
+        }
+
+        private void bAcceder_Click(object sender, EventArgs e)
+        {
+            int numUsuario;
+            if (validar_EditText(Correo) && validar_EditText(Contrasena))
+            {
+                correo2 = Correo.Text;
+                contrasena2 = Contrasena.Text;
+                numUsuario = API0.User.login(correo2, contrasena2);
+                if (numUsuario == 0)
+                {
+                    ///APPDIALOG: ENVIAR MENSAJE QUE ESTA MAL
+                }
+                else
+                {
+                    appCode.ChangeLoginStatus(this, 1);
+                    appCode.SaveUser(this, numUsuario);
+                }
+            }
+        }
+
+        public bool validar_EditText(EditText t)
+        {
+            bool v = false;
+            if (t.Text == "" || t.Text == null)
+            {
+                t.SetBackgroundColor(Android.Graphics.Color.Orange);
+                Android.App.AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                AlertDialog alertDialog = builder.Create();
+                alertDialog.SetTitle("Acceder");
+                alertDialog.SetMessage("Te faltan algunos campos !");
+                alertDialog.SetButton("OK", (s, ev) =>
+                {
+                });
+                alertDialog.Show();
+            }
+
+            else
+            {
+                v = true;
+            }
+            return v;
         }
     }
 }
