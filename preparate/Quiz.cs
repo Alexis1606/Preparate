f575f2bf1c178e41f4616ad1c6337cad4471cc87
alexis1606 <alexis_1606@hotmail.com>
2017-05-07 01:26:42 -0500

    Ya muestra preunta con imagen

--- preparate/Quiz.cs
+++ preparate/Quiz.cs
@@ -1,18 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using System.Timers;
 using API0;
 using Classes;
 using Android.Preferences;
+using Android.Graphics;
+using System.Net;
+
+
 
 namespace preparate
 {
@@ -280,10 +279,34 @@ namespace preparate
                 imagen.Visibility = ViewStates.Gone;
             }else
             {
+
+
+
                 imagen.Visibility = ViewStates.Visible;
-                imagen.SetImageURI(Android.Net.Uri.Parse(p.imagen));
+                var imageBitmap = GetImageBitmapFromUrl(p.imagen);
+                imagen.SetImageBitmap(imageBitmap);
+
+
+                //Android.Net.Uri u = Android.Net.Uri.Parse(p.imagen);
+                //imagen.SetImageURI(Android.Net.Uri.Parse(p.imagen));
             }
             
+        }
+
+        private Bitmap GetImageBitmapFromUrl(string url)
+        {
+            Bitmap imageBitmap = null;
+
+            using (var webClient = new WebClient())
+            {
+                var imageBytes = webClient.DownloadData(url);
+                if (imageBytes != null && imageBytes.Length > 0)
+                {
+                    imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+                }
+            }
+
+            return imageBitmap;
         }
 
         private int validarRespuesta(Pregunta p, int respuesta)
