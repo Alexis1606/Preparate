4a338aa2703490e07846ca652d67fa3b10451c91
Alexis1606 <alexis_1606@hotmail.com>
2017-05-15 23:04:57 -0500

    LeaderBoard

--- preparate/leaderboard.cs
+++ preparate/leaderboard.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Preferences;
 
 namespace preparate
 {
@@ -16,12 +17,78 @@ namespace preparate
     public class leaderboard : Activity
     {
         Spinner s1;
+        EditText et;
+        int user;
+        API0.TipoExamen[] te;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.leaderboard);
+            ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(this);
+            user = prefs.GetInt("user", 0);
+            s1 = FindViewById<Spinner>(Resource.Id.spinner1);
+            et = FindViewById<EditText>(Resource.Id.editText1);
+
+            te = API0.TipoExamen.allbyuser(user);
+            var items = new List<string>()
+            { };
+            foreach (API0.TipoExamen t in te)
+            {
+                items.Add(t.desc);
+            }
+            var adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, items);
+            s1.Adapter = adapter;
+            s1.ItemSelected += spinner1_ItemSelected;
+
+
+
+
+
+        }
+        private void spinner1_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+
+            et.Text = API0.Leaderboard.get_leadboard(te[e.Position].id);
+
+            ////Spinner spinner = (Spinner)sender;
+
+            ////string toast = string.Format("El tipo de examen es {0}", spinner.GetItemAtPosition(e.Position));
+            ////Toast.MakeText(this, toast, ToastLength.Long).Show();
+            //int tipo = te[e.Position].id;
+            //API0.Certificadores[] cert = API0.Certificadores.all();
+            //API0.Materia[] mat = API0.Materia.all();
+            //var items = new List<string>()
+            //{ };
+            //switch (tipo)
+            //{
+            //    case 0:
+            //        items.Add("-");
+            //        s2.Enabled = false;
+            //        break;
+            //    case 5:
+            //        foreach (API0.Certificadores t in cert)
+            //        {
+            //            items.Add(t.desc);
+            //        }
+
+            //        s2.Enabled = true;
+            //        break;
+            //    case 6:
+            //        foreach (API0.Materia t in mat)
+            //        {
+            //            items.Add(t.desc);
+            //        }
+            //        s2.Enabled = true;
+            //        break;
+            //}
+            //var adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, items);
+            //s2.Adapter = adapter;
+            //s2.ItemSelected += spinner2_ItemSelected;
+
+
+
 
         }
 
