a7f08ec0a942fecdfd23a1b147b3b2a043bc79c0
Juan Luis Aguilar Badillo <jhony_primitivo@hotmail.com>
2017-05-17 09:55:37 -0500

    Notificaciones

--- preparate/Quiz.cs
+++ preparate/Quiz.cs
@@ -10,7 +10,8 @@ using Classes;
 using Android.Preferences;
 using Android.Graphics;
 using System.Net;
-
+using System.Threading.Tasks;
+using Firebase.Iid;
 
 namespace preparate
 {
@@ -48,6 +49,18 @@ namespace preparate
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.Quiz);
+
+
+            if (!GetString(Resource.String.google_app_id).Equals("1:593192999279:android:7fd609f7126dc407"))
+                throw new System.Exception("Invalid Json file");
+            Task.Run(() =>
+            {
+                var instanceId = FirebaseInstanceId.Instance;
+                instanceId.DeleteInstanceId();
+                Android.Util.Log.Debug("TAG", "{0} {1}", instanceId.Token, instanceId.GetToken(GetString(Resource.String.gcm_defaultSenderId), Firebase.Messaging.FirebaseMessaging.InstanceIdScope));
+            });
+
+
 
             //spinner1 = FindViewById<Spinner>(Resource.Id.spinner1);
             //spinner1.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
