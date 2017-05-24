7d8a3299da801455e19b1556e4e0c4876b58a1f2
Juan Luis Aguilar Badillo <jhony_primitivo@hotmail.com>
2017-05-24 12:31:07 -0500

    Validar Fecha

--- preparate/Crear_Perfil.cs
+++ preparate/Crear_Perfil.cs
@@ -60,92 +60,127 @@ namespace preparate
 
         private void BValidar_Click(object sender, EventArgs e)
         {
+            string cadena = Convert.ToString(txtFechaNac.Text);
+            string[] separadas;
+            separadas = cadena.Split(',');
+            string dia = separadas[0];
+            string mes = separadas[1];
+            string año = separadas[2];
+            Android.App.AlertDialog.Builder builder10 = new AlertDialog.Builder(this);
+            AlertDialog alertDialog10 = builder10.Create();
+            alertDialog10.SetTitle("REGISTRO");
 
-            obtener_datos();
-            int genero;
-
-            if (validar_contra() && validar_EditText(txtNombre) && validar_EditText(txtApellidos) && validar_EditText(txtEmail) && txtFechaNac.Text != "" && validarMail(txtEmail))
+            if (Convert.ToInt32(año) > 2017)
             {
-                if (GeneroMasculino.Checked)
+                alertDialog10.SetIcon(Resource.Drawable.Icon);
+                alertDialog10.SetMessage("¿Acaso vienes del Futuro?");
+                alertDialog10.SetButton("OK", (s, ev) =>
                 {
-                    genero = 1;
-                }
-                else
-                {
-                    genero = 0;
-                }
 
-                Android.App.AlertDialog.Builder builder5 = new Android.App.AlertDialog.Builder(this);
-            Android.App.AlertDialog alerDialog5 = builder5.Create();
-            //Titulo
-            alerDialog5.SetTitle("Compra");
-            //Icono
-            alerDialog5.SetIcon(Resource.Drawable.Icon);
-            //Pregunta
-            alerDialog5.SetMessage("Al dar Click en continuar aceptas nuestro Aviso de Privacidad.\n el cual lo puedes consultar en: www.preparate.com/AvisoDePrivacidad");
-                alerDialog5.SetButton("Continuar", (si, eve) =>
-                {
-                    StartActivity(typeof(Crear_Perfil));
-                   
                 });
-            
-            alerDialog5.SetButton3("Cancelar", (se, eve) =>
+                alertDialog10.Show();
+            }
+            else if (Convert.ToInt32(año) >= 2014 && Convert.ToInt32(año) <= 2017)
             {
-                try
+                alertDialog10.SetIcon(Resource.Drawable.Icon);
+                alertDialog10.SetMessage("Eres Un bebe muy inteligente, pero no te puedes registrar");
+                alertDialog10.SetButton("OK", (s, ev) =>
                 {
 
-                    string res = API0.User.InsertUser(txtNombre.Text, txtApellidos.Text, "", Convert.ToDateTime(txtFechaNac.Text), tContra1.Text, txtEmail.Text, genero, 1, 1, "a");
-                    Android.App.AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    AlertDialog alertDialog = builder.Create();
-                    alertDialog.SetTitle("REGISTRO");
-                    switch (res)
-                    {
-                        case "-2":
-                            txtEmail.SetBackgroundColor(Android.Graphics.Color.Red);
-                            alertDialog.SetMessage("La dirección de correo ya existe, por favor ingresa otra dirección ó inicia sesión.");
-                            alertDialog.SetButton("OK", (s, ev) =>
-                            {
-                            });
-                            alertDialog.Show();
-                            break;
-                        default:
+                });
+                alertDialog10.Show();
+            }
 
-                            alertDialog.SetIcon(Resource.Drawable.Icon);
-                            alertDialog.SetMessage("Registro Exitoso");
-                            alertDialog.SetButton("OK", (s, ev) =>
-                            {
-                                StartActivity(typeof(MenuPrincipal));
-                                Finish();
-                            });
-                            alertDialog.Show();
-                            break;
+            else if (Convert.ToInt32(año) > 1917 && Convert.ToInt32(año) < 2013 )
+            {
+                obtener_datos();
+                int genero;
+
+                if (validar_contra() && validar_EditText(txtNombre) && validar_EditText(txtApellidos) && validar_EditText(txtEmail) && txtFechaNac.Text != "" && validarMail(txtEmail))
+                {
+                    if (GeneroMasculino.Checked)
+                    {
+                        genero = 1;
+                    }
+                    else
+                    {
+                        genero = 0;
                     }
 
+                    Android.App.AlertDialog.Builder builder5 = new Android.App.AlertDialog.Builder(this);
+                    Android.App.AlertDialog alerDialog5 = builder5.Create();
+                    //Titulo
+                    alerDialog5.SetTitle("Compra");
+                    //Icono
+                    alerDialog5.SetIcon(Resource.Drawable.Icon);
+                    //Pregunta
+                    alerDialog5.SetMessage("Al dar Click en continuar aceptas nuestro Aviso de Privacidad.\n el cual lo puedes consultar en: www.preparate.com/AvisoDePrivacidad");
+                    alerDialog5.SetButton("Continuar", (si, eve) =>
+                    {                        
+
+                        try
+                        {
+
+                            string res = API0.User.InsertUser(txtNombre.Text, txtApellidos.Text, "", Convert.ToDateTime(txtFechaNac.Text), tContra1.Text, txtEmail.Text, genero, 1, 1, "a");
+                            Android.App.AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            AlertDialog alertDialog = builder.Create();
+                            alertDialog.SetTitle("REGISTRO");
+                            switch (res)
+                            {
+                                case "-2":
+                                    txtEmail.SetBackgroundColor(Android.Graphics.Color.Red);
+                                    alertDialog.SetMessage("La dirección de correo ya existe, por favor ingresa otra dirección ó inicia sesión.");
+                                    alertDialog.SetButton("OK", (s, ev) =>
+                                    {
+                                    });
+                                    alertDialog.Show();
+                                    break;
+                                default:
+
+                                    alertDialog.SetIcon(Resource.Drawable.Icon);
+                                    alertDialog.SetMessage("Registro Exitoso");
+                                    alertDialog.SetButton("OK", (s, ev) =>
+                                    {
+                                        StartActivity(typeof(MenuPrincipal));
+                                        Finish();
+                                    });
+                                    alertDialog.Show();
+                                    break;
+                            }
 
 
-                }
 
-                catch (Exception ex)
-                {
+                        }
 
-                }
+                        catch (Exception ex)
+                        {
 
-            });
-            alerDialog5.Show();
+                        }
 
+                    });
 
+                    alerDialog5.SetButton3("Cancelar", (se, eve) =>
+                    {
+                        StartActivity(typeof(Crear_Perfil));
 
-
-
-
-
-
-
-
-
+                    });
+                    alerDialog5.Show();
 
                 }
             }
+            else
+            {
+                alertDialog10.SetIcon(Resource.Drawable.Icon);
+                alertDialog10.SetMessage("Eres muy Anciano para usar esta aplicación.");
+                alertDialog10.SetButton("OK", (s, ev) =>
+                {
+
+                });
+                alertDialog10.Show();
+            }
+
+
+        }
 
         public void obtener_datos()
         {
